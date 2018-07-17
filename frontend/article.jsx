eb7afc6e3b44044a9d39d6d44448722046b93ab4
Stephen Kang <kang.stephen94@gmail.com>
2018-07-16 20:37:43 -0700

    Add links to title

--- frontend/article.jsx
+++ frontend/article.jsx
@@ -3,13 +3,15 @@ export const Article = (props) => {
     const title = article.title,
        description = article.description,
        image = article.image_url,
-       authors = article.authors;
+       authors = article.authors,
+       link = article.url;
 
     return (
         <li className='article'>
-            <h1>{title}</h1>
+            <h1><a href={link}>{title}</a></h1>
             <img src={image}></img>
             <p>{description}</p>
+            <p>{authors}</p>
         </li>
     )
 }
