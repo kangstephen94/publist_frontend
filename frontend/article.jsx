38a968c3847b84c89e42d860e8ff66b76c1cb601
Stephen Kang <kang.stephen94@gmail.com>
2018-07-16 20:46:27 -0700

    Format data

--- frontend/article.jsx
+++ frontend/article.jsx
@@ -3,15 +3,16 @@ export const Article = (props) => {
     const title = article.title,
        description = article.description,
        image = article.image_url,
-       authors = article.authors,
        link = article.url;
+
 
     return (
         <li className='article'>
+            <div className='title'>
             <h1><a href={link}>{title}</a></h1>
+            </div>
             <img src={image}></img>
             <p>{description}</p>
-            <p>{authors}</p>
         </li>
     )
 }
