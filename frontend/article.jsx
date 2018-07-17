a43b5c3a8e9f325ec097af9fa1b6b95e22ef81cb
Stephen Kang <kang.stephen94@gmail.com>
2018-07-17 00:44:35 -0700

    Overflow factor

--- frontend/article.jsx
+++ frontend/article.jsx
@@ -1,15 +1,23 @@
 export const Article = (props) => {
     const { article } = props;
-    const title = article.title,
+    let title = article.title,
        description = article.description,
        image = article.image_url,
-       link = article.url;
+       link = article.url,
+       author = article.authors;
 
+       {if (author) {
+        author = <p>By: {author.filter(word => word.length < 25).join(',  ')}</p>
+       } else {
+           author = <p></p>
+       }
+    }
 
     return (
         <li className='article'>
             <div className='title'>
             <h1><a target="_blank" href={link}>{title}</a></h1>
+            {author}
             </div>
             <a target="_blank" href={link}><img src={image}></img></a>
             <p>{description}</p>
